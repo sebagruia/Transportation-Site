ef5f0fdc7c379f8e026647c0e8f2e938bc6d2806
sebagruia <sebastiangruia@gmail.com>
2020-01-28 12:13:35 +0100

    Added scrolling effect

--- js/app.js
+++ js/app.js
@@ -3,13 +3,20 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    
+
     //  Scroll on buttons using Jquery
 
-     $('.scroll-to-despre-noi').click(function () {
+    $('.scroll-to-home').click(function (event) {
+        console.log(event)
+        $('html, body').animate({
+            scrollTop: $('#hero').offset().top
+        },1000);
+    });
+    $('.scroll-to-despre-noi').click(function () {
+        console.log(event)
         $('html, body').animate({
             scrollTop: $('.despre-noi').offset().top
-        }, 1000);
+        },1000);
     });
 
 
@@ -24,40 +31,41 @@ document.addEventListener('DOMContentLoaded', () => {
             scrollTop: $('.tarife-tractari').offset().top
         }, 1000);
     });
+    $('.scroll-to-contact').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.suntem-aici').offset().top
+        }, 1000);
+    });
 
-  
-            // Adding Background Parallax to devices with width > 576px
-            const width = window.innerWidth;
-            if(width>576){
-                const hero = document.getElementById('hero');
-                window.addEventListener('scroll', (e) => {
-                    if (hero.getAttribute("data-type") === 'background') {
-                        const test = hero.getAttribute('data-speed');
-                        let yPos = -(window.pageYOffset / parseInt(test));
-                        // Put together our final background position
-                        let coords = `50% ${yPos}px`;
-                        // Move the background
-                        hero.setAttribute("style", `background-position: ${coords};`);
-                    }
-                });
-            }
 
-            // Removing navbar-brand text for width > 992px
-            if(width>=992){
-                const navbarBrandText = document.querySelector('.navbar-brand-text');
-                navbarBrandText.innerText = '';            
+    // Adding Background Parallax to devices with width > 576px
+    const width = window.innerWidth;
+    if (width > 576) {
+        const hero = document.getElementById('hero');
+        window.addEventListener('scroll', (e) => {
+            if (hero.getAttribute("data-type") === 'background') {
+                const test = hero.getAttribute('data-speed');
+                let yPos = -(window.pageYOffset / parseInt(test));
+                // Put together our final background position
+                let coords = `50% ${yPos}px`;
+                // Move the background
+                hero.setAttribute("style", `background-position: ${coords};`);
             }
-            
+        });
+    }
+
+    // Removing navbar-brand text for devices with width > 992px
+    if (width >= 992) {
+        const navbarBrandText = document.querySelector('.navbar-brand-text');
+        navbarBrandText.innerText = '';
+    }
+
 
     // Hamburger Functionality 
     const hamburger = document.querySelector('.hamburger');
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('is-active');
     });
-
-
-    
-
 
 
 });
